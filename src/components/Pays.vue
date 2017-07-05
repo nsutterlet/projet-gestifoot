0f820e944e1bd69930c24c6b615c488cb2c22ea1
hgoncalves <hugomanu.goncalve@he-arc.ch>
2017-07-05 23:49:43 +0200

    Update Pays.vue liens ajoutés lien de toutes les ligues

--- src/components/Pays.vue
+++ src/components/Pays.vue
@@ -14,31 +14,31 @@
                     </md-button>
                 </md-layout>
                 <md-layout md-flex=" 35 ">
-                    <md-button>
+                    <md-button v-on:click.native="$router.push({name:'League', params:{idLeague:'439'}})">
                         <span class="flag-icon flag-icon-pt "></span>
                     </md-button>
                 </md-layout>
             </md-layout>
             <md-layout md-align="center " md-gutter="16 ">
                 <md-layout md-flex="35 ">
-                    <md-button>
+                    <md-button v-on:click.native="$router.push({name:'League', params:{idLeague:'426'}})">
                         <span class="flag-icon flag-icon-gb "></span>
                     </md-button>
                 </md-layout>
                 <md-layout md-flex="35 ">
-                    <md-button>
+                    <md-button v-on:click.native="$router.push({name:'League', params:{idLeague:'436'}})">
                         <span class="flag-icon flag-icon-es "></span>
                     </md-button>
                 </md-layout>
             </md-layout>
             <md-layout md-align="center " md-gutter="16 ">
                 <md-layout md-flex="35 ">
-                    <md-button>
-                        <span class="flag-icon flag-icon-ge "></span>
+                    <md-button v-on:click.native="$router.push({name:'League', params:{idLeague:'430'}})">
+                        <span class="flag-icon flag-icon-de "></span>
                     </md-button>
                 </md-layout>
                 <md-layout md-flex="35 ">
-                    <md-button>
+                    <md-button v-on:click.native="$router.push({name:'League', params:{idLeague:'438'}})">
                         <span class="flag-icon flag-icon-it "></span>
                     </md-button>
                 </md-layout>
