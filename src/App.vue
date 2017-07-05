5ef28658cc111cdf2988ef61baf227f003600ba8
nsutterlet <n.sutterlet@outlook.com>
2017-07-06 01:12:00 +0200

    Ajout classement dans League.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
 <template>
-    <div id="app">
+  <div id="app">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Pacifico|Vibur:300,400,500,700,400italic">
+    <link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="manifest" href="static/manifest.json">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="application-name" content="Shop Memo">
+    <meta name="apple-mobile-web-app-title" content="Shop Memo">
+    <meta name="theme-color" content="#388E3C">
+    <meta name="msapplication-navbutton-color" content="#388E3C">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="msapplication-starturl" content="/">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="icon" type="image/png" sizes="192x192" href="static/icons/logo-192.png">
+    <link rel="apple-touch-icon" href="static/icons/logo-192.png">
+
+
+  
         <md-toolbar>
             <h2 class="md-title" style="flex: 1">GestiFoot</h2>
         </md-toolbar>
--- src/components/League.vue
+++ src/components/League.vue
@@ -6,10 +6,60 @@
             </md-card-header>
             <md-card-content>
                 <md-layout md-align="center" md-gutter="16">
-                    <md-list>
-                        <md-list-item> 1. PSG</md-list-item>
-                    </md-list>
+    
+                    <md-table>
+                        <md-table-header>
+                            <md-table-row>
+                                <!--<md-table-head ></md-table-head>-->
+                                <md-table-head>Position</md-table-head>
+                                <md-table-head>Equipe</md-table-head>
+                                <md-table-head>J</md-table-head>
+                                <md-table-head>V</md-table-head>
+                                <md-table-head>N</md-table-head>
+                                <md-table-head>D</md-table-head>
+                                <md-table-head>BM</md-table-head>
+                                <md-table-head>BE</md-table-head>
+                                <md-table-head>Point</md-table-head>
+                            </md-table-row>
+                        </md-table-header>
+    
+                        <md-table-body>
+                            <md-table-row v-for="(team,index) in league.standing" :team="team" v-bind:key="index">
+                                <!--<md-table-cell><img src="{{team.crestURI}}" alt="team" width="15px" height="15px"></md-table-cell>-->
+                                <md-table-cell>{{ team.position }}</md-table-cell>
+                                md-button v-on:click.native="$router.push({name:'Team', params:{idTeam:'434'}})">
+                                <md-table-cell>{{ team.teamName }}</md-table-cell>
+                                
+                                </md-button>
+    
+                                <md-table-cell>{{ team.playedGames }}</md-table-cell>
+                                <md-table-cell>{{ team.wins}}</md-table-cell>
+                                <md-table-cell>{{ team.draws }}</md-table-cell>
+                                <md-table-cell>{{ team.losses }}</md-table-cell>
+                                <md-table-cell>{{ team.goals}}</md-table-cell>
+                                <md-table-cell>{{ team.goalsAgainst }}</md-table-cell>
+                                <md-table-cell>{{ team.points}}</md-table-cell>
+                            </md-table-row>
+                        </md-table-body>
+                    </md-table>
                 </md-layout>
+<!--                <md-layout md-align="center" md-gutter="16">
+    
+                    <md-table>
+                        </md-table-header>
+    
+                        <md-table-body>
+                            <md-table-row v-for="(game,index) in journey.standing" :team="team" v-bind:key="index">
+                                <!--<md-table-cell><img src="{{team.crestURI}}" alt="team" width="15px" height="15px"></md-table-cell>
+                                <md-table-cell>{{ game.home}}</md-table-cell>
+                                <md-table-cell>{{ game.score }}</md-table-cell>
+                                <md-table-cell> - </md-table-cell>
+                                <md-table-cell>{ game.score }}</md-table-cell>
+                                <md-table-cell>{{ game.away }}</md-table-cell>
+                            </md-table-row>
+                        </md-table-body>
+                    </md-table>
+                </md-layout>-->
             </md-card-content>
         </md-card>
     </div>
@@ -24,8 +74,10 @@ const maConfig = { headers: { 'X-Auth-Token': '7b8a71dc7eec4bbca11371f65c4e95ff'
 export default {
     data () {
         this.getLeague();
+       // this.getJourney();
         return {
-            league: {}
+            league: {},
+            journey: {}
         };
     },
     name: 'league',
@@ -35,10 +87,22 @@ export default {
                 this.league = response.data;
             });
         }
+        // getJourney (matchday) {
+        //     axios.get(`http://api.football-data.org/v1/competitions/${this.$route.params.idLeague}/leagueTable/?matchday=${this.league.matchday}`, maConfig).then((response) => {
+        //         this.journey = response.data;
+        //     });
+        // }
     }
 };
 </script>
 
 <style>
+.md-title {
+    text-align: center;
+}
 
+.thd {
+    text-align: center;
+    Font-Weight: Bold;
+}
 </style>
