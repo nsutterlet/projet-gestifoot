64e5038abcdec0eea1c3d804fd92774a5fb84801
hgoncalves <hugomanu.goncalve@he-arc.ch>
2017-07-06 08:45:37 +0200

    Update : resize menu images

--- src/components/Pays.vue
+++ src/components/Pays.vue
@@ -1,44 +1,41 @@
 <template>
     <div class="pays">
         <md-card>
-            <md-card-header>
-                <div class="md-title">Football Stats</div>
-            </md-card-header>
             <md-card-content>
     
                 <md-layout md-align="center " md-gutter="16 ">
                     <md-layout md-flex="35 ">
                         <md-button v-on:click.native="$router.push({name:'League', params:{idLeague:'434'}})">
-                            <img src="../assets/ligue_1.svg" alt="logo">
+                            <img class="img-league" src="../assets/ligue_1.svg" alt="logo">
                         </md-button>
                     </md-layout>
                     <md-layout md-flex="35 ">
                         <md-button v-on:click.native="$router.push({name:'League', params:{idLeague:'439'}})">
-                            <img src="../assets/liga_nos.svg" alt="logo">
+                            <img class="img-league" src="../assets/liga_nos.svg" alt="logo">
                         </md-button>
                     </md-layout>
                 </md-layout>
                 <md-layout md-align="center " md-gutter="16 ">
                     <md-layout md-flex="35 ">
                         <md-button v-on:click.native="$router.push({name:'League', params:{idLeague:'426'}})">
-                            <img src="../assets/premier_league.svg" alt="logo">
+                            <img class="img-league" src="../assets/premier_league.svg" alt="logo">
                         </md-button>
                     </md-layout>
                     <md-layout md-flex="35 ">
                         <md-button v-on:click.native="$router.push({name:'League', params:{idLeague:'436'}})">
-                            <img src="../assets/la_liga.png" alt="logo">
+                            <img class="img-league" src="../assets/la_liga.png" alt="logo">
                         </md-button>
                     </md-layout>
                 </md-layout>
                 <md-layout md-align="center " md-gutter="16 ">
                     <md-layout md-flex="35 ">
                         <md-button v-on:click.native="$router.push({name:'League', params:{idLeague:'430'}})">
-                            <img src="../assets/bundesliga.svg" alt="logo">
+                            <img class="img-league" src="../assets/bundesliga.svg" alt="logo">
                         </md-button>
                     </md-layout>
                     <md-layout md-flex="35 ">
                         <md-button v-on:click.native="$router.push({name:'League', params:{idLeague:'438'}})">
-                            <img src="../assets/serie_a.svg" alt="logo">
+                            <img class="img-league" src="../assets/serie_a.svg" alt="logo">
                         </md-button>
                     </md-layout>
                 </md-layout>
